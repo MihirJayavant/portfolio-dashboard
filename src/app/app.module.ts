ea29e18f5017c7ccc1be1fcd5a2e8fe74e1f3d76
Mihir Jayavant <mihir.jayavant2@gmail.com>
2021-05-23 18:07:46 +0530

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { ChipsModule } from 'primeng/chips';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    ChipsModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomepageComponent},
+      {path: '', redirectTo: '/home', pathMatch: 'full'},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
