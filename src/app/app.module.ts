31f9c0cb8b8fd3f4f686f26dab2ab9de57cf69bf
Mihir Jayavant <mihir.jayavant2@gmail.com>
2021-06-14 23:45:52 +0530

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,32 @@
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
-import { ChipsModule } from 'primeng/chips';
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { RouterModule } from '@angular/router'
+import { ButtonModule } from 'primeng/button'
+import { InputTextModule } from 'primeng/inputtext'
 
-import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
+import { AppComponent } from './app.component'
+import { HomepageComponent } from './homepage/homepage.component'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomepageComponent
-  ],
+  declarations: [AppComponent, HomepageComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    ChipsModule,
     FormsModule,
+    ButtonModule,
+    InputTextModule,
     RouterModule.forRoot([
-      {path: 'home', component: HomepageComponent},
-      {path: '', redirectTo: '/home', pathMatch: 'full'},
-    ])
+      { path: 'home', component: HomepageComponent },
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
+      },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
